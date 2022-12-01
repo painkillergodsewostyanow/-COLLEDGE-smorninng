b640c725f9fa5d14b36ac25ad2934338a0dbb39f
painkillergodsewostyanow <thebladethatbringsdeath@gmail.com>
2022-12-01 22:58:02 +0300

    123
    коминтнул перед тем как трогать сложную тему что бы не навернуть проект

--- Smorning/app/src/main/java/com/example/smorning/Quiz.kt
+++ Smorning/app/src/main/java/com/example/smorning/Quiz.kt
@@ -1,28 +1,40 @@
 package com.example.smorning
 
 import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smorning.databinding.ActivityQuizBinding
 
 class Quiz : AppCompatActivity() {
-    lateinit var binding: ActivityQuizBinding
-    var firstChoice = createChoice()
-    var secondChoice = createChoice()
-    var answerSelect= listOf(firstChoice.second, secondChoice.second).random()
-    var count = 3
+    private lateinit var binding: ActivityQuizBinding
+    private var firstChoice = createChoice()
+    private var secondChoice = createChoice()
+    private var answerSelect= listOf(firstChoice.second, secondChoice.second).random()
+    private var count = 3
+    lateinit var signal: MediaPlayer
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        signal = MediaPlayer.create(this,R.raw.first_alarm_sing)
+        signal.isLooping = true
+        signal.start()
         binding = ActivityQuizBinding.inflate(layoutInflater)
         binding.chose1.text = firstChoice.first + "   " +firstChoice.second.toString()
         binding.chose2.text = secondChoice.first + "   " +secondChoice.second.toString()
         binding.answer.text = "= $answerSelect"
         binding.counter.text = "Решите $count, чтобы остановить сигнал"
         setContentView(binding.root)
+        binding.root.setOnClickListener {
+            window.decorView.apply {
+                // Hides the navigation bar.
+                systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            }
+        }
         supportActionBar?.hide()
     }
+
     @SuppressLint("SetTextI18n")
     fun quiz(){
         firstChoice = createChoice()
@@ -33,6 +45,16 @@ class Quiz : AppCompatActivity() {
         binding.answer.text = "= $answerSelect"
         binding.counter.text = "Решите $count, чтобы остановить сигнал"
     }
+
+
+    fun finishAllProcess(){
+        signal.stop()
+        finish()
+    }
+
+    override fun onBackPressed() {
+
+    }
     fun choiceFirst(view: View){
         if (firstChoice.second == answerSelect){
             count --
@@ -41,13 +63,12 @@ class Quiz : AppCompatActivity() {
             count ++
         }
         if (count == 0){
-            finish()
+            finishAllProcess()
         }
         quiz()
 
     }
     fun choiceSecond(view: View){
-
         if (secondChoice.second == answerSelect){
             count --
         }
@@ -55,16 +76,16 @@ class Quiz : AppCompatActivity() {
             count ++
         }
         if (count == 0){
-            finish()
+            finishAllProcess()
         }
         quiz()
     }
     fun stopQuiz(view: View){
-        finish()
+        finishAllProcess()
     }
     // создаем рандомный пример и возвращаем его, в виде строки и ответ
-    fun createChoice(): Pair<String, Int> {
-        val listOperation = listOf<String>("-","+")
+    private fun createChoice(): Pair<String, Int> {
+        val listOperation = listOf("-","+")
         val x = (0..20).random()
         val y = (0..20).random()
         val z = (0..20).random()
